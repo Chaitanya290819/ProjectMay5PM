a0aa18fa1bd251f2cf3af7f42802c658559db4f8
Chaitanya290819 <bisariachaitanya@gmail.com>
2020-05-25 22:33:04 +0530

    done code

--- src/test/java/org/tcs/A.java
+++ src/test/java/org/tcs/A.java
@@ -3,8 +3,7 @@ package org.tcs;
 public class A {
 
 	public static void main(String[] args) {
-		System.out.println("hiiiiii");
-		System.out.println("vhggj");
+		System.out.println("hello");
 	}
 
 }
